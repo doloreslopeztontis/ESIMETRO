e9f187e38685731ee1691937c7ae46a667b7ea03
Federico <fedewalo@gmail.com>
2019-06-08 14:49:28 -0300

    Empiezo a hacer la primera pantalla

---
+++ Php/Bienvenidos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="../Css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../Css/Estilos.css">
+    <title>Bienvenido/a</title>
+</head>
+<body>
+    <div class="">
+        <img src="../Imagenes/banerEsi.png" alt="Baner Esi" class="col-md-12 Baner">
+    </div>
+    <div class="">
+        <h2 class="Titulo">BIENVENIDO/A</h2>
+    </div>
+    <div>
+        <p class="Texto">Descripción de la aplicación, como funciona para que sirve cual es el objetivo...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu.</p>
+    </div>
+    <div class="">
+        <p class="Boton" id="botonComenzar">COMENZAR</p>
+    </div>
+
+</body>
+</html>
