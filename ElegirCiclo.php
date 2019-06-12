3dae65218bdcc98c8ecbe62eb7c6252957ff35f3
FEDERICO JOEL WALOVNIK <43403835@ort>
2019-06-12 09:05:08 -0300

    include baner

--- ElegirCiclo.php
+++ ElegirCiclo.php
@@ -13,11 +13,7 @@
     <title>Elegir Ciclo</title>
 </head>
 <body>
-    <div class="container-fluid ContenedorBaner">
-       <div class="row">
-            <a href="https://campus.ort.edu.ar/educacionsexualintegral" target="_blank"><img src="img/banerEsi.png" alt="Baner Esi" class="col-md-12 Baner"></a>
-       </div>
-    </div>
+    <?php include("Baner.php")?>
 
    <div class="container-fluid">
         <div class="ContenedorPregunta">
---
+++ Pregunta.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/estilosGenerales.css">
+
+    <title>Pregunta</title>
+</head>
+<body>
+    <?php 
+    include("Baner.php");
+    $Respuestas = array("Educacion Sexual Integral","Encantado Sin Imagenes","Encantado Sin Imagenes","Encantado Sin Imagenes", "0"));
+
+    $Preguntas = array("Que significa las siglas ESI?", "Cuando se creo la ley de la ESI?");
+    
+    var $Contador=0;
+
+    ?>
+    <div class="container-fluid">
+        <div class="ContenedorPregunta">
+            <div class="row col-md-12 justify-content-center FondoBlanco">
+                <p class="Pregunta center"><?php echo $Preguntas[$Contador]?></p>
+            </div>
+            <div class="row col-md-12 FondoBlanco">
+            <?php for(int i=0; i<4;i++): ?>
+                <button type="button" class="btn btn-outline-secondary Respuesta"><?php $Respuestas[$Contador][i].pregunta ?></button>
+            <?php endfor?>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
