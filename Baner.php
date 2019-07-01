f74217fb4c11971d8fe788b2b0037edb1d26a7f1
FEDERICO JOEL WALOVNIK <43403835@ort>
2019-07-01 14:20:02 -0300

    Baner

--- Baner.php
+++ Baner.php
@@ -1,5 +1,5 @@
 <div class="container-fluid ContenedorBaner">
     <div class="row">
-        <a href="https://campus.ort.edu.ar/educacionsexualintegral" target="_blank"><img src="img/banerEsi.png" alt="Baner Esi" class="col-md-12 Baner"></a>
+        <a href="https://campus.ort.edu.ar/educacionsexualintegral" target="_blank"><img src="img/BanerEsi2.jpg" alt="Baner Esi" class="col-md-12 Baner"></a>
     </div>
 </div>
