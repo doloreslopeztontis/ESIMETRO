40d459d21e99265deb664b156fc4c259685b8a67
DOLORES LOPEZ JOFFRE <43992757@comunidadort.edu.ar>
2019-07-19 12:06:33 -0300

    1 pregunta insertada

--- esimetro_db.sql
+++ esimetro_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 19-07-2019 a las 14:46:50
+-- Tiempo de generación: 19-07-2019 a las 15:05:43
 -- Versión del servidor: 5.7.21
 -- Versión de PHP: 5.6.35
 
@@ -25,38 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `respuestas`
+-- Estructura de tabla para la tabla `preguntas`
 --
 
-DROP TABLE IF EXISTS `respuestas`;
-CREATE TABLE IF NOT EXISTS `respuestas` (
-  `opcion` int(11) NOT NULL,
-  `idPregunta` int(11) NOT NULL,
-  `respuesta` varchar(100) NOT NULL,
-  `ponderacion` int(11) NOT NULL,
-  PRIMARY KEY (`opcion`,`idPregunta`),
-  KEY `fkPregunta` (`idPregunta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `preguntas`;
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `idPregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `idCategoria` int(11) NOT NULL,
+  `pregunta` varchar(500) NOT NULL,
+  `texto_final` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idPregunta`),
+  KEY `fkCategoria` (`idCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `respuestas`
+-- Volcado de datos para la tabla `preguntas`
 --
 
-INSERT INTO `respuestas` (`opcion`, `idPregunta`, `respuesta`, `ponderacion`) VALUES
-(1, 1, 'Homero', 100),
-(2, 1, 'Eduardo Galeano', 0),
-(3, 1, 'Borges', 0),
-(4, 1, 'Maria Elena Walsh', 0);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  ADD CONSTRAINT `respuestas_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`idPregunta`) ON DELETE CASCADE ON UPDATE CASCADE;
+INSERT INTO `preguntas` (`idPregunta`, `idCategoria`, `pregunta`, `texto_final`) VALUES
+(3, 1, '¿Cómo te gustaría que se vista la conductora de un programa de TV?', 'textofinal - TV'),
+(4, 1, '¿Qué harías si en un programa de TV actual el conductor le corta la pollera a las participantes?', 'textofinal - PARTICIPANTES'),
+(5, 1, 'En tu última publicación de instragram tuviste solo 3 likes, Cómo reaccionas?', 'textofinal - INSTAGRAM'),
+(6, 1, 'Uso las redes sociales para…', 'textofinal - REDES SOCIALES'),
+(7, 1, 'Viene una amiga y te plantea que le gusta Mecatrónica pero no la elige porque “no hay pibas” ¿Qué le decís?', 'textofinal - MECATRONICA');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
